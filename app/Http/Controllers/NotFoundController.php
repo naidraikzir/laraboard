7b1e07d34cce646ef6341570af908fe1125ae8a1
Rizki Ardian <radeite8@gmail.com>
2017-09-22 10:15:27 +0700

    Removed Illuminate\Http\Request imports in Controllers

--- app/Http/Controllers/NotFoundController.php
+++ app/Http/Controllers/NotFoundController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
 class NotFoundController extends Controller
 {
     public function index()
