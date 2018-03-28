b64e0534c554f9bd6c8f90980064f8df8ce97d90
Rizki Ardian <radeite8@gmail.com>
2018-03-28 22:16:38 +0700

    Removed PageA route and view

--- resources/assets/js/menus.js
+++ resources/assets/js/menus.js
@@ -1,7 +1,6 @@
 export default [
   { text: 'Main', title: true },
   { path: '/', text: 'Dashboard', exact: true },
-  { path: '/page-a', text: 'Page A' },
   { text: 'Miscellaneous', title: true },
   { path: '/components', text: 'Components' },
   { path: '/404', text: 'Not Found' },
--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -4,7 +4,6 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 const Index = () => import(/* webpackChunkName: 'dashboard-index' */ 'vue/pages/Index');
-const PageA = () => import(/* webpackChunkName: 'page-a' */ 'vue/pages/PageA');
 const Components = () => import(/* webpackChunkName: 'components' */ 'vue/pages/Components');
 const NotFound = () => import(/* webpackChunkName: 'page-a' */ 'vue/pages/404');
 
@@ -14,7 +13,6 @@ export default new VueRouter({
   base: '/dashboard/',
   routes: [
     { path: '', name: 'Laraboard', component: Index },
-    { path: '/page-a', name: 'Page A', component: PageA },
     { path: '/components', name: 'Components', component: Components },
     { path: '/404', name: 'Not Found', component: NotFound },
     { path: '/*', redirect: { name: 'Not Found' } },
